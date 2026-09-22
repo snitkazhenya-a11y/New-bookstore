9dba5b6298228ec6f5724691567552015c2a3082
snitk <snitka.zhenya@gmail.com>
2026-09-22 22:55:02 +0300

    to apdate cach

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404, redirect, render
+from django.shortcuts import render, redirect, get_object_or_404
 from catalog.models import Book
 
 def cart_detail(request):
@@ -8,15 +8,29 @@ def cart_detail(request):
 def cart_add(request, book_id):
     cart = request.session.get('cart', {})
     book_id_str = str(book_id)
+    quantity = int(request.POST.get('quantity', 1))
+    book = get_object_or_404(Book, id=book_id)
+
     if book_id_str in cart:
-        cart[book_id_str]['quantity'] += 1
+        cart[book_id_str]['quantity'] += quantity
     else:
         book = get_object_or_404(Book, id=book_id)
         cart[book_id_str] = {
             'title': book.title,
             'price': str(book.price),
-            'quantity': 1,
+            'quantity': quantity,
         }
     request.session['cart'] = cart
     request.session.modified = True
+    return redirect('cart:cart_detail')
+
+def cart_remove(request, book_id):
+    cart = request.session.get('cart', {})
+    book_id_str = str(book_id)
+
+    if book_id_str in cart:
+        del cart[book_id_str]
+        request.session['cart'] = cart
+        request.session.modified = True
+
     return redirect('cart:cart_detail')
